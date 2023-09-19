59bbf581cc56bf3325c98230cff7a45e9515bdf6
sruthichennoju <98538367+sruthichennoju@users.noreply.github.com>
2023-09-19 10:12:43 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,80 @@
-<h1>Welcome to Heroku !!!</h1>
-<h2>Sruthi Chennoju- Onboarding App </h2>
-<h3>My First App...Rock it!!!</h3>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Delicious Food</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <h1>Welcome to Delicious Food</h1>
+        <nav>
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#menu">Menu</a></li>
+                <li><a href="#about">About Us</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <section id="home">
+        <h2>Discover Our Delicious Cuisine</h2>
+        <p>Indulge in a world of flavors and culinary delights.</p>
+        <a href="#menu" class="cta-button">Explore Our Menu</a>
+    </section>
+
+    <section id="menu">
+        <h2>Our Menu</h2>
+        <ul>
+            <li>
+                <h3>Appetizers</h3>
+                <ul>
+                    <li>Appetizer 1</li>
+                    <li>Appetizer 2</li>
+                    <li>Appetizer 3</li>
+                </ul>
+            </li>
+            <li>
+                <h3>Main Courses</h3>
+                <ul>
+                    <li>Main Course 1</li>
+                    <li>Main Course 2</li>
+                    <li>Main Course 3</li>
+                </ul>
+            </li>
+            <li>
+                <h3>Desserts</h3>
+                <ul>
+                    <li>Dessert 1</li>
+                    <li>Dessert 2</li>
+                    <li>Dessert 3</li>
+                </ul>
+            </li>
+        </ul>
+    </section>
+
+    <section id="about">
+        <h2>About Us</h2>
+        <p>We are passionate about serving delicious food made with fresh, high-quality ingredients. Our chefs are dedicated to creating memorable dining experiences for our guests.</p>
+    </section>
+
+    <section id="contact">
+        <h2>Contact Us</h2>
+        <p>If you have any questions or would like to make a reservation, please get in touch with us.</p>
+        <address>
+            <p>Email: <a href="mailto:info@deliciousfood.com">info@deliciousfood.com</a></p>
+            <p>Phone: +1-123-456-7890</p>
+        </address>
+    </section>
+
+    <footer>
+        <p>&copy; 2023 Delicious Food</p>
+    </footer>
+</body>
+</html>
+
 
 
 
