7a49aa1a2fefc63575ba1386441b8ee9db9e3843
sruthichennoju <98538367+sruthichennoju@users.noreply.github.com>
2025-02-03 18:14:43 +0530

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
             <ul>
                 <li><a href="#home">Home</a></li>
                 <li><a href="#menu">Menu</a></li>
-                <li><a href="#about">About Us</a></li>
+                <li><a href="#about">About US</a></li>
                 <li><a href="#contact">Contact</a></li>
             </ul>
         </nav>
