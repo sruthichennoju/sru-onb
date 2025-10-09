06da5ab79b2342cc9b2d40b2ceeb790984f220a0
sruthichennoju <98538367+sruthichennoju@users.noreply.github.com>
2025-10-09 13:37:13 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Delicious Food</title>
+    <title>Delicious FooD</title>
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
